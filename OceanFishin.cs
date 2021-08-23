a0cadd7e7a80effc59f76063d20e031e621fc15c
Mark Sosnowski <markjsosnowski@gmail.com>
2021-08-23 17:43:14 -0400

    Add project files.

---
+++ OceanFishin.cs
@@ -0,0 +1,184 @@
+﻿using Dalamud.Game.Command;
+using Dalamud.Plugin;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+using System;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+
+namespace OceanFishin
+{
+    public class OceanFishin : IDalamudPlugin
+    {
+        
+        enum time : int
+        {
+            Unknown  = -1,
+            Day = 0,
+            Sunset = 1,
+            Night = 2
+        }
+        enum bait : int
+        {
+            Unknown = -1,
+            PlumpWorm = 0,
+            Ragworm = 1,
+            Krill = 2
+        }
+        public string Name => "Ocean Fishin'";
+       
+        private const string command_name = "/oceanfishin";
+        private const string alt_command_1 = "/oceanfishing";
+        private const string alt_command_2 = "/bait";
+
+        private bool on_boat = false;
+
+        private DalamudPluginInterface pi;
+        private OceanFishinUI ui;
+
+        //Format = Location:[Start, Intuition, Spectral Day, Spectral Sunset, Spectral Night]
+        private Dictionary<string, int[]> bait_dict = new Dictionary<string, int[]>()
+        {
+            {"Galadion Bay",                    new int[] {(int)bait.PlumpWorm, (int)bait.Krill, (int)bait.Ragworm, (int)bait.PlumpWorm, (int)bait .Krill} },
+            {"The Southern Strait of Merlthor", new int[] {(int)bait.Krill, (int)bait.PlumpWorm, (int)bait.Krill, (int)bait.Ragworm, (int)bait.PlumpWorm} },
+            {"The Northern Strait of Merlthor", new int[] {(int)bait.Ragworm, (int)bait.Ragworm, (int)bait.PlumpWorm, (int)bait.Ragworm, (int)bait.Krill} },
+            {"Rhotano Sea",                     new int[] {(int)bait.PlumpWorm, (int)bait.Krill, (int)bait.PlumpWorm, (int)bait.Ragworm, (int)bait.Krill} },
+            {"The Cieldalaes",                  new int[] {(int)bait.Ragworm, (int)bait.Krill, (int)bait.Krill, (int)bait.PlumpWorm, (int)bait.Krill} },
+            {"The Bloodbrine Sea",              new int[] {(int)bait.Krill, (int)bait.Krill, (int)bait.Ragworm, (int)bait.PlumpWorm, (int)bait.Krill} },
+            {"The Rothlyt Sound",               new int[] {(int)bait.PlumpWorm, (int)bait.Ragworm, (int)bait.Krill, (int)bait.Krill, (int)bait.Krill} },
+            {default_location,                new int[] {(int)bait.Unknown, (int)bait.Unknown, (int)bait.Unknown, (int)bait.Unknown, (int)bait.Unknown} },
+        };
+
+        private const int endevor_territory_type = 900;
+        private const int location_textnode_index = 20;
+        private const int day_imagenode_index = 24;
+        private const int sunset_imagenode_index = 23;
+        private const int night_imagenode_index = 22;
+        private const int expected_nodelist_count = 24;
+        private const string default_location = "Unknown Location";
+
+
+        /* Three image nodes make up the time of day indicator.
+         They all use the same texture, but the part_id determines
+         which part of the texture is used. So the node has one part id if
+         it's lit up (active) and another when it's dim (inactive).
+         Day Active = 9, Day Inactive = 4
+         Sunset Active = 10, Sunset Inactive = 5
+         Night Active = 11, Night Inactive = 6 */
+        private const int day_icon_lit = 9;
+        private const int sunset_icon_lit = 10;
+        private const int night_icon_lit = 11;
+
+        public string AssemblyLocation { get => assemblyLocation; set => assemblyLocation = value; }
+        private string assemblyLocation = System.Reflection.Assembly.GetExecutingAssembly().Location;
+
+        public void Dispose()
+        {
+            this.ui.Dispose();
+            this.pi.CommandManager.RemoveHandler(command_name);
+            this.pi.CommandManager.RemoveHandler(alt_command_1);
+            this.pi.CommandManager.RemoveHandler(alt_command_2);
+            this.pi.Dispose();
+        }
+
+        public void Initialize(DalamudPluginInterface pluginInterface)
+        {
+            this.pi = pluginInterface ?? throw new ArgumentNullException(nameof(pluginInterface), "DalamudPluginInterface cannot be null");
+            this.ui = new OceanFishinUI();
+
+            this.pi.CommandManager.AddHandler(command_name, new CommandInfo(OnCommand)
+            {
+                HelpMessage = "Opens the ocean fishing bait chart."
+            }) ;
+            this.pi.CommandManager.AddHandler(alt_command_1, new CommandInfo(OnCommand)
+            {
+                HelpMessage = "Alias for /oceanfishin"
+            });
+            this.pi.CommandManager.AddHandler(alt_command_2, new CommandInfo(OnCommand)
+            {
+                HelpMessage = "Alias for /oceanfishin"
+            });
+
+            this.pi.UiBuilder.OnBuildUi += DrawUI;
+        }
+
+        private void OnCommand(string command, string args)
+        {
+            // in response to the slash command, just display our main ui
+            this.ui.Visible = true;
+        }
+
+        private bool check_location()
+        {
+            if ((int)pi.ClientState.TerritoryType == endevor_territory_type)
+                return true;
+            else
+                return false;
+        }
+
+        private unsafe (string, int) get_data()
+        {
+            string current_location = default_location;
+            int current_time = (int)time.Unknown;
+            var addon_ptr = pi.Framework.Gui.GetUiObjectByName("IKDFishingLog", 1);
+            if(addon_ptr == null || addon_ptr == IntPtr.Zero)
+            {
+                return (current_location, current_time);
+            }
+            AtkUnitBase* addon = (AtkUnitBase*)addon_ptr;
+            
+            ////Without this check, the plugin might try to get a child node before the list was 
+            ///populated and cause a null pointer exception. 
+            if(addon->UldManager.NodeListCount  < expected_nodelist_count)
+            {
+                return (current_location, current_time);
+            }
+            current_location = get_location(addon);
+            current_time = get_time(addon);
+            return (current_location, current_time);
+        }
+        
+        private unsafe string get_location(AtkUnitBase* ptr)
+        {
+            if (ptr == null)
+                return default_location;
+            AtkResNode* res_node = ptr->UldManager.NodeList[location_textnode_index];
+            AtkTextNode* text_node = (AtkTextNode*)res_node;
+            return Marshal.PtrToStringAnsi(new IntPtr(text_node->NodeText.StringPtr));
+        }
+
+        private unsafe int get_time(AtkUnitBase* ptr)
+        {
+            if (ptr == null)
+                return (int)time.Unknown;
+            AtkResNode* res_node = ptr->UldManager.NodeList[day_imagenode_index];
+            AtkImageNode* image_node = (AtkImageNode*)res_node;
+            if (image_node->PartId == day_icon_lit)
+                return (int)time.Day;
+            res_node = ptr->UldManager.NodeList[sunset_imagenode_index];
+            image_node = (AtkImageNode*)res_node;
+            if (image_node->PartId == sunset_icon_lit)
+                return (int)time.Sunset;
+            res_node = ptr->UldManager.NodeList[night_imagenode_index];
+            image_node = (AtkImageNode*)res_node;
+            if (image_node->PartId == night_icon_lit)
+                return (int)time.Night;
+            return (int)time.Unknown;
+        }
+
+        private void DrawUI()
+        {
+            string location = default_location;
+            int time = (int)OceanFishin.time.Unknown;
+            on_boat = check_location();
+            if (on_boat)
+            {
+                (location, time) = get_data();
+            }
+            //This usually isn't a problem but just here for safety.
+            if(bait_dict.ContainsKey(location))
+            {
+                this.ui.Draw(on_boat, location, time, bait_dict[location]);
+            }
+        }
+    }
+}
---
+++ OceanFishinUI.cs
@@ -0,0 +1,89 @@
+﻿using ImGuiNET;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace OceanFishin
+{
+    internal class OceanFishinUI : IDisposable
+    {
+        private Dictionary<int, string> bait_strings = new Dictionary<int, string>()
+        {
+            {-1, "Unknown Bait"},
+            {1, "Ragworm"},
+            {2, "Krill"},
+            {0, "Plump Worm"},
+        };
+        private Dictionary<int, string> time_strings = new Dictionary<int, string>()
+        {
+            {-1, "Unknown Time" },
+            {0, "during the Day" },
+            {1, "at Sunset" },
+            {2, "at Night"}
+        };
+
+
+        enum bait_index : int
+        {
+            starter = 0,
+            intuition = 1,
+            spectral_day = 2,
+            spectral_offset = 2,
+            spectral_sunset = 3,
+            spectral_night = 4
+        }
+
+        // this extra bool exists for ImGui, since you can't ref a property
+        private bool visible = false;
+        public bool Visible
+        {
+            get { return this.visible; }
+            set { this.visible = value; }
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public void Draw(bool on_boat, string location, int time, int[] bait_list)
+        {
+            // This is our only draw handler attached to UIBuilder, so it needs to be
+            // able to draw any windows we might have open.
+            // Each method checks its own visibility/state to ensure it only draws when
+            // it actually makes sense.
+            // There are other ways to do this, but it is generally best to keep the number of
+            // draw delegates as low as possible.
+
+            DrawMainWindow(on_boat, location, time, bait_list);
+        }
+
+        public void DrawMainWindow(bool on_boat, string location, int time, int[] bait_list)
+        {
+            if (!Visible)
+            {
+                return;
+            }
+
+            ImGui.SetNextWindowSize(new Vector2(250, 150), ImGuiCond.FirstUseEver);
+            ImGui.SetNextWindowSizeConstraints(new Vector2(250, 150), new Vector2(float.MaxValue, float.MaxValue));
+
+
+            if (ImGui.Begin("Ocean Fishin'", ref this.visible, ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse))
+            {
+                if (on_boat)
+                {
+                    ImGui.Text("You are aboard The Endeavor, sailing in " + location + " " + time_strings[time] + ".");
+                    ImGui.Text("The ideal bait for this area and time is: ");
+                    ImGui.Text("Starting Bait → " + bait_strings[bait_list[(int)bait_index.starter]]);
+                    ImGui.Text("Fisher's Intution → " + bait_strings[bait_list[(int)bait_index.intuition]]);
+                    ImGui.Text("Spectral Current → " + bait_strings[bait_list[(int)bait_index.spectral_offset + time]]);
+                }
+                else
+                {
+                    ImGui.Text("Board The Endeavor to update the bait list.");
+                }
+
+            }
+        }
+    }
+}
